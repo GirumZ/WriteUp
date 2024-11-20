b5ca3b2bbbbc12d7bbb1399ea0ec85ae9dc3a515
GirumZ <girum.zebe@gmail.com>
2024-11-20 16:18:28 +0300

    [Update]: Added settings for SMTP server

--- myblog/settings.py
+++ myblog/settings.py
@@ -122,3 +122,21 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# SMTP server
+
+import environ
+import os
+
+# Initialize environment variables
+env = environ.Env()
+env.read_env(env_file=os.path.join(BASE_DIR, '.env'))  # Automatically loads variables from a .env file
+
+print("EMAIL_HOST_USER:", env('EMAIL_HOST_USER', default=None))  # Debug: print value
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
